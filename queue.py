2eb491046d62ab690ea73f01909f9e95a78126aa
mathamp <jwzz123123@gmail.com>
2021-03-13 02:47:27 +0900

    feat : Implement deque based Waiting queue

---
+++ queue.py
@@ -0,0 +1,31 @@
+from typing import List
+from collections import deque
+
+
+class WaitingQueue(deque):
+
+    def __init__(self):
+        super().__init__()
+
+    def is_able_to_start(self, num: int) -> bool:
+        return len(self) >= num
+
+    def wait(self, player_id: int) -> bool:
+        if player_id not in self:
+            self.append(player_id)
+            return True
+        else:
+            return False
+
+    def pop_players(self, num: int) -> List[int]:
+        if self.is_able_to_start(num):
+            return [self.popleft() for _ in range(num)]
+        else:
+            return list()
+
+    def cancel_wait(self, player_id: int) -> bool:
+        try:
+            self.remove(player_id)
+            return True
+        except ValueError:
+            return False
