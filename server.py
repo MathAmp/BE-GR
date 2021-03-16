6916e87c5fb9e76632ad2cf2733bb5bb7205de4e
mathamp <jwzz123123@gmail.com>
2021-03-16 23:54:41 +0900

    feat : Implement Room process according to game engine

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from typing import List, Deque, Dict, AnyStr, Tuple, Union, Optional
+from typing import List, Deque, Dict, AnyStr, Tuple, Union, Optional, Type
 from collections import deque, namedtuple
-from enum import auto, Enum
+from enum import auto, Enum, IntEnum
 import json
 
 from queue import WaitingQueue
@@ -8,7 +8,7 @@ from queue import WaitingQueue
 
 Command = AnyStr
 SendScheme = namedtuple("SendScheme", ['to', 'cmd'])
-RoomProcessResult = namedtuple("RoomProcessResult", ['call_type', 'retval'])
+ProcessResult = namedtuple("RoomProcessResult", ['call_type', 'retval'])
 
 
 class CallType(Enum):
@@ -44,6 +44,10 @@ class Suit:
         pass
 
 
+class Play:
+    pass
+
+
 class FriendCall:
 
     def __init__(self, fctype: int, card: Optional[Card] = None):
@@ -64,6 +68,13 @@ class Engine:
     def get_next_call(self) -> CallType:
         return CallType.FRIEND_CALL
 
+    def get_legal_plays(self, player_id: int) -> List[Play]:
+        pass
+
+    @property
+    def next_player(self):
+        return int()
+
     def bidding(self, bidder: int, trump: Suit, bid: int) -> int:
         """
         suit
@@ -71,13 +82,13 @@ class Engine:
         """
         raise NotImplementedError
 
-    def exchange(self, discarding_cards: list) -> int:
+    def exchange(self, player: int, discarding_cards: list) -> int:
         """
         Card List
         """
         raise NotImplementedError
 
-    def trump_change(self, trump: Suit) -> int:
+    def trump_change(self, player: int, trump: Suit) -> int:
         """
         suit
         """
@@ -89,7 +100,7 @@ class Engine:
         """
         raise NotImplementedError
 
-    def friend_call(self, friend_call: FriendCall) -> int:
+    def friend_call(self, player: int, friend_call: FriendCall) -> int:
         """
         fctype
         card
@@ -97,84 +108,141 @@ class Engine:
         """
         raise NotImplementedError
 
-    def play(self, play: int) -> int:
+    def play(self, play: Play) -> int:
         raise NotImplementedError
+
+
+class InvalidCallTypeError(Exception):
+
+    def __init__(self, call_type):
+        super().__init__(f"Invalid call type : {call_type}")
+
+
+class ErrorCode(Enum):
+
+    NO_PLAYER_IN_ROOM = auto()
+    INVALID_DATA = auto()
+    INDEX_OUT_OF_RANGE = auto()
+    INVALID_PLAYER_TURN = auto()
+
+
+class DataKey(IntEnum):
+    bid = 0
+    suit = auto()
+    card = auto()
+    card_list = auto()
+    miss_deal = auto()
+    friend_call_type = auto()
+    play_idx = auto()
 
 
 class Room:
 
     regular_personnel = 5
+    ProcessReturnType = Union[ProcessResult, ErrorCode]
 
     def __init__(self, account_id_list: List[int]):
         self.state = list()
         self.account_id_tuple: Tuple[int] = tuple(account_id_list)
         self.game_engine = Engine()
 
-    def process(self, account_id: int, data: dict) -> Union[RoomProcessResult, None]:
+    def process(self, account_id: int, data: dict) -> ProcessReturnType:
         player_id = self._account_id_to_player_id(account_id)
         if player_id is not None:
             return self._process_game(player_id, data)
+        else:
+            return ErrorCode.NO_PLAYER_IN_ROOM
 
-    def _process_game(self, player_id: int, data: dict) -> Union[RoomProcessResult, None]:
+    def _process_game(self, player_id: int, data: dict) -> ProcessReturnType:
         current_call_type = self.game_engine.get_next_call()
         if current_call_type == CallType.BID:
-            self._bidding(self.game_engine, player_id, data)
+            return self._bidding(self.game_engine, player_id, data)
         elif current_call_type == CallType.EXCHANGE:
-            self._exchange(self.game_engine, data)
+            return self._exchange(self.game_engine, player_id, data)
         elif current_call_type == CallType.TRUMP_CHANGE:
-            self._trump_change(self.game_engine, data)
+            return self._trump_change(self.game_engine, player_id, data)
         elif current_call_type == CallType.MISS_DEAL_CHECK:
-            self._miss_deal_check(self.game_engine, player_id, data)
+            return self._miss_deal_check(self.game_engine, player_id, data)
         elif current_call_type == CallType.FRIEND_CALL:
-            self._friend_call(self.game_engine, data)
+            return self._friend_call(self.game_engine, player_id, data)
         elif current_call_type == CallType.PLAY:
-            self._play(self.game_engine, data)
+            return self._play(player_id, data)
         else:
-            return None
-
-    @staticmethod
-    def _bidding(engine: Engine, player_id: int, data: dict) -> Union[RoomProcessResult, None]:
-        if 'suit' in data and 'bid' in data and Suit.is_suitstr(data['suit']):
-            trump = Suit.str_to_suit(data['suit'])
-            bid = data['bid']
-            return_value = engine.bidding(player_id, trump, bid)
-            return RoomProcessResult(CallType.BID, return_value)
-
-    @staticmethod
-    def _exchange(engine: Engine, data: dict) -> Union[RoomProcessResult, None]:
-        if 'card_list' in data and all(Card.is_cardstr(card) for card in data['card_list']):
-            card_list = [Card.str_to_card(card) for card in data['card_list']]
-            return_value = engine.exchange(card_list)
-            return RoomProcessResult(CallType.EXCHANGE, return_value)
-
-    @staticmethod
-    def _trump_change(engine: Engine, data: dict) -> Union[RoomProcessResult, None]:
-        if 'suit' in data and Suit.is_suitstr(data['suit']):
-            trump = Suit.str_to_suit(data['suit'])
-            return_value = engine.trump_change(trump)
-            return RoomProcessResult(CallType.TRUMP_CHANGE, return_value)
-
-    @staticmethod
-    def _miss_deal_check(engine: Engine, player_id: int, data: dict) -> Union[RoomProcessResult, None]:
-        if 'miss_deal' in data:
-            miss_deal = data['miss_deal']
-            return_value = engine.miss_deal_check(player_id, miss_deal)
-            return RoomProcessResult(CallType.MISS_DEAL_CHECK, return_value)
-
-    @staticmethod
-    def _friend_call(engine: Engine, data: dict) -> Union[RoomProcessResult, None]:
-        if 'fctype' in data and 'card' in data and Card.is_cardstr(data['card']):
-            fctype = data['fctype']
-            card = Card.str_to_card(data['card'])
-            friend_call = FriendCall(fctype, card)
-            return_value = engine.friend_call(friend_call)
-            return RoomProcessResult(CallType.FRIEND_CALL, return_value)
+            raise InvalidCallTypeError(current_call_type)
 
     @classmethod
-    def _play(cls, engine: Engine, data: dict) -> Union[RoomProcessResult, None]:
-        if 'player_id' in data and cls.is_valid_player_id(data['player_id'], cls.regular_personnel):
-            player_id = data['player_id']
-            # How can determine play instance?
+    def _bidding(cls, engine: Engine, player_id: int, data: dict) -> ProcessReturnType:
+        if cls._is_valid_data(DataKey.bid, data, int) and \
+                cls._is_valid_data(DataKey.suit, data, str) and Suit.is_suitstr(data[DataKey.suit]):
+            trump: Suit = Suit.str_to_suit(data[DataKey.suit])
+            bid: int = data[DataKey.bid]
+            return_value = engine.bidding(player_id, trump, bid)
+            return ProcessResult(CallType.BID, return_value)
+        else:
+            return ErrorCode.INVALID_DATA
+
+    @classmethod
+    def _exchange(cls, engine: Engine, player_id: int, data: dict) -> ProcessReturnType:
+        if cls._is_valid_data(DataKey.card_list, data, list) and \
+                all(Card.is_cardstr(card) for card in data[DataKey.card_list]):
+            card_list: List[Card] = [Card.str_to_card(card) for card in data[DataKey.card_list]]
+            return_value = engine.exchange(player_id, card_list)
+            return ProcessResult(CallType.EXCHANGE, return_value)
+        else:
+            return ErrorCode.INVALID_DATA
+
+    @classmethod
+    def _trump_change(cls, engine: Engine, player_id: int, data: dict) -> ProcessReturnType:
+        if cls._is_valid_data(DataKey.suit, data, str) and Suit.is_suitstr(data[DataKey.suit]):
+            trump = Suit.str_to_suit(data[DataKey.suit])
+            return_value = engine.trump_change(player_id, trump)
+            return ProcessResult(CallType.TRUMP_CHANGE, return_value)
+        else:
+            return ErrorCode.INVALID_DATA
+
+    @classmethod
+    def _miss_deal_check(cls, engine: Engine, player_id: int, data: dict) -> ProcessReturnType:
+        if cls._is_valid_data(DataKey.miss_deal, data, bool):
+            miss_deal = data[DataKey.miss_deal]
+            return_value = engine.miss_deal_check(player_id, miss_deal)
+            return ProcessResult(CallType.MISS_DEAL_CHECK, return_value)
+        else:
+            return ErrorCode.INVALID_DATA
+
+    @classmethod
+    def _friend_call(cls, engine: Engine, player_id: int, data: dict) -> ProcessReturnType:
+        if cls._is_valid_data(DataKey.friend_call_type, data, int) and \
+                cls._is_valid_data(DataKey.card, data, str) and Card.is_cardstr(data[DataKey.card]):
+            friend_call_type = data[DataKey.friend_call_type]
+            card = Card.str_to_card(data[DataKey.card])
+
+            # Make friend call and execute
+            friend_call = FriendCall(friend_call_type, card)
+            return_value = engine.friend_call(player_id, friend_call)
+            return ProcessResult(CallType.FRIEND_CALL, return_value)
+        else:
+            return ErrorCode.INVALID_DATA
+
+    @classmethod
+    def _play(cls, engine: Engine, player_id: int, data: dict) -> ProcessReturnType:
+        if engine.next_player == player_id:
+            play_list: List[Play] = engine.get_legal_plays(player_id)
+            if cls._is_valid_data(DataKey.play_idx, data, int):
+                play_idx = data[DataKey.play_idx]
+                if 0 <= play_idx < len(play_list):
+                    play = play_list[play_idx]
+                    return_value = engine.play(play)
+                    return ProcessResult(CallType.PLAY, return_value)
+                else:
+                    return ErrorCode.INDEX_OUT_OF_RANGE
+            else:
+                return ErrorCode.INVALID_DATA
+        else:
+            return ErrorCode.INVALID_PLAYER_TURN
+
+    @staticmethod
+    def _is_valid_data(key: int, data: dict, key_type: Type):
+        return key in data and type(data[key]) is key_type
 
     @staticmethod
     def is_valid_player_id(player_id: int, total_id_cnt: int) -> bool:
@@ -260,11 +328,11 @@ class Server:
         return new_room
 
     @classmethod
-    def front_end_to_back_end(cls):
+    def front_end_to_back_end(cls) -> str:
         raise NotImplementedError
 
     @classmethod
-    def back_end_to_front_end(cls, player_id: int, cmd: Command):
+    def back_end_to_front_end(cls, player_id: int, cmd: Command) -> bool:
         raise NotImplementedError
 
 
